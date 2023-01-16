4240514eea4eec25b6c0232f48f986c55fca62b6
Abg Nazmi Syahirul <syahirul26@gmail.com>
2023-01-16 10:07:59 +0800

    update

--- enhance/address.js
+++ enhance/address.js
@@ -1,46 +1,71 @@
 "use strict";
 exports.__esModule = true;
 exports.Address = void 0;
+var fs = require("fs");
 var Address = /** @class */ (function () {
     function Address(address) {
+        this.loadData();
         this.tokenize(address);
     }
+    //if the cities, states and streets have a bigger data, asynchronous method should be applied
+    // to read them asynchronously
+    Address.prototype.loadData = function () {
+        //console.log("Loading data from text file ...");
+        try {
+            // read the data from the .txt file
+            // read the data from the cities.txt file
+            var citiesData = fs.readFileSync("cities.txt", "utf8");
+            // read the data from the states.txt file
+            var statesData = fs.readFileSync("states.txt", "utf8");
+            // read the data from the streets.txt file
+            var streetsData = fs.readFileSync("streets.txt", "utf8");
+            // split the data by newline to get an array of strings
+            this.cities = citiesData.split("\n");
+            this.states = statesData.split("\n");
+            this.streets = streetsData.split("\n");
+            //console.log("Data loaded successfully");
+        }
+        catch (err) {
+            console.error(err);
+        }
+    };
     Address.prototype.tokenize = function (address) {
         // regular expressions for each address component
-        var aptNumberRegex = /No\s\d+/gi;
-        var cityRegex = /(kuala terengganu|kuala lumpur|kajang|bangi|damansara|petaling jaya|puchong|subang jaya|cyberjaya|putrajaya|mantin|kuching|seremban)/gi;
-        var stateRegex = /(selangor|terengganu|pahang|kelantan|melaka|pulau pinang|kedah|johor|perlis|sabah|sarawak)/gi;
+        var aptNumberRegex = /^(no\s)?(\d+)/gi;
+        var cityRegex = new RegExp("(".concat(this.cities.join("|"), ")"), "gi");
+        var stateRegex = new RegExp("(".concat(this.states.join("|"), ")"), "gi");
         var postcodeRegex = /\b[0-9]{5}\b/g;
-        var streetRegex = /(jalan|jln|lorong|persiaran)\s/gi;
+        var streetRegex = new RegExp("(".concat(this.streets.join("|"), ")"), "gi");
         var temp = address;
         // match each component in the address string
+        //console.log("matching aptNumber ...");
         var match = aptNumberRegex.exec(temp);
         if (match) {
             this.aptNumber = match[0];
-            temp = temp.replace(this.aptNumber, '');
+            temp = temp.replace(this.aptNumber, "");
         }
         match = cityRegex.exec(temp);
         if (match) {
             this.city = match[0];
-            temp = temp.replace(this.city, '');
+            temp = temp.replace(this.city, "");
         }
         match = stateRegex.exec(temp);
         if (match) {
             this.state = match[0];
-            temp = temp.replace(this.state, '');
+            temp = temp.replace(this.state, "");
         }
         match = postcodeRegex.exec(temp);
         if (match) {
             this.postcode = match[0];
-            temp = temp.replace(this.postcode, '');
+            temp = temp.replace(this.postcode, "");
         }
         match = streetRegex.exec(temp);
         if (match) {
             this.street = match[0];
-            temp = temp.replace(this.street, '');
+            temp = temp.replace(this.street, "");
         }
         // everything else is considered as section
-        this.section = temp.replace(/[^a-zA-Z\s]/g, '').trim();
+        this.section = temp.replace(/[^a-zA-Z\s]/g, "").trim();
     };
     return Address;
 }());
--- enhance/address.ts
+++ enhance/address.ts
@@ -1,52 +1,84 @@
+import * as fs from "fs";
+
 export class Address {
-    aptNumber: string;
-    city: string;
-    state: string;
-    postcode: string;
-    street: string;
-    section: string;
+  aptNumber: string;
+  city: string;
+  state: string;
+  postcode: string;
+  street: string;
+  section: string;
 
-    constructor(address: string) {
-        this.tokenize(address);
+  private cities: string[];
+  private states: string[];
+  private streets: string[];
+
+  constructor(address: string) {
+    this.loadData();
+    this.tokenize(address);
+  }
+
+  //if the cities, states and streets have a bigger data, asynchronous method should be applied
+  // to read them asynchronously
+
+  private loadData() {
+    //console.log("Loading data from text file ...");
+    try {
+      // read the data from the .txt file
+      // read the data from the cities.txt file
+      const citiesData = fs.readFileSync("cities.txt", "utf8");
+      // read the data from the states.txt file
+      const statesData = fs.readFileSync("states.txt", "utf8");
+      // read the data from the streets.txt file
+      const streetsData = fs.readFileSync("streets.txt", "utf8");
+
+      // split the data by newline to get an array of strings
+      this.cities = citiesData.split("\n");
+      this.states = statesData.split("\n");
+      this.streets = streetsData.split("\n");
+      //console.log("Data loaded successfully");
+    } catch (err) {
+      console.error(err);
     }
+  }
 
-    private tokenize(address: string) {
-        // regular expressions for each address component
-        const aptNumberRegex = /No\s\d+/gi;
-        const cityRegex = /(kuala terengganu|kuala lumpur|kajang|bangi|damansara|petaling jaya|puchong|subang jaya|cyberjaya|putrajaya|mantin|kuching|seremban)/gi;
-        const stateRegex = /(selangor|terengganu|pahang|kelantan|melaka|pulau pinang|kedah|johor|perlis|sabah|sarawak)/gi;
-        const postcodeRegex = /\b[0-9]{5}\b/g;
-        const streetRegex = /(jalan|jln|lorong|persiaran)\s/gi;
+  private tokenize(address: string) {
+    // regular expressions for each address component
+    const aptNumberRegex = /^(no\s)?(\d+)/gi;
+    const cityRegex = new RegExp(`(${this.cities.join("|")})`, "gi");
+    const stateRegex = new RegExp(`(${this.states.join("|")})`, "gi");
+    const postcodeRegex = /\b[0-9]{5}\b/g;
+    const streetRegex = new RegExp(`(${this.streets.join("|")})`, "gi");
 
-        let temp = address;
+    let temp = address;
 
-        // match each component in the address string
-        let match = aptNumberRegex.exec(temp);
-        if (match) {
-            this.aptNumber = match[0];
-            temp = temp.replace(this.aptNumber,'');
-        }
-        match = cityRegex.exec(temp);
-        if (match) {
-            this.city = match[0];
-            temp = temp.replace(this.city,'');
-        }
-        match = stateRegex.exec(temp);
-        if (match) {
-            this.state = match[0];
-            temp = temp.replace(this.state,'');
-        }
-        match = postcodeRegex.exec(temp);
-        if (match) {
-            this.postcode = match[0];
-            temp = temp.replace(this.postcode,'');
-        }
-        match = streetRegex.exec(temp);
-        if (match) {
-            this.street = match[0];
-            temp = temp.replace(this.street,'');
-        }
-        // everything else is considered as section
-        this.section = temp.replace(/[^a-zA-Z\s]/g,'').trim();
+    // match each component in the address string
+    //console.log("matching aptNumber ...");
+    let match = aptNumberRegex.exec(temp);
+    if (match) {
+      this.aptNumber = match[0];
+      temp = temp.replace(this.aptNumber, "");
     }
+    match = cityRegex.exec(temp);
+    if (match) {
+      this.city = match[0];
+      temp = temp.replace(this.city, "");
+    }
+    match = stateRegex.exec(temp);
+    if (match) {
+      this.state = match[0];
+      temp = temp.replace(this.state, "");
+    }
+    match = postcodeRegex.exec(temp);
+    if (match) {
+      this.postcode = match[0];
+      temp = temp.replace(this.postcode, "");
+    }
+    match = streetRegex.exec(temp);
+    if (match) {
+      this.street = match[0];
+      temp = temp.replace(this.street, "");
+    }
+    // everything else is considered as section
+    this.section = temp.replace(/[^a-zA-Z\s]/g, "").trim();
+  }
 }
